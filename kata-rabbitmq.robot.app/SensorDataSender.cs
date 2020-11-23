9f98d9f7115acaa7352a726c9bd082a4d4dd8898
Stefan Boos <stefan.boos@gmx.de>
2020-11-23 05:32:47 +0100

    refactor: introduce single level of abstraction in SensorDataSender

--- kata-rabbitmq.robot.app/SensorDataSender.cs
+++ kata-rabbitmq.robot.app/SensorDataSender.cs
@@ -10,6 +10,8 @@ namespace kata_rabbitmq.robot.app
     public class SensorDataSender : BackgroundService
     {
         private readonly ILogger<SensorDataSender> _logger;
+        private IConnection _connection;
+        private IModel _channel;
 
         public SensorDataSender(ILogger<SensorDataSender> logger)
         {
@@ -18,54 +20,13 @@ namespace kata_rabbitmq.robot.app
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _logger.LogInformation("Waiting for cancellation request");
-            stoppingToken.Register(() => _logger.LogInformation("STOP request received"));
-            stoppingToken.ThrowIfCancellationRequested();
-
-            IModel channel = null;
-            IConnection connection = null;
-
             try
             {
+                RegisterCancellationRequest(stoppingToken);
+                
                 while (true)
                 {
-                    if (channel == null)
-                    {
-                        _logger.LogDebug("Connecting to RabbitMQ ...");
-                        try
-                        {
-                            var connectionFactory = new ConnectionFactory();
-                            connectionFactory.HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOSTNAME");
-                            var portString = Environment.GetEnvironmentVariable("RABBITMQ_PORT");
-                            if (portString != null) connectionFactory.Port = int.Parse(portString);
-                            connectionFactory.UserName = Environment.GetEnvironmentVariable("RABBITMQ_USERNAME");
-                            connectionFactory.Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD");
-                            connectionFactory.VirtualHost = "/";
-                            connectionFactory.ClientProvidedName = "app:robot";
-                            
-                            _logger.LogDebug($"RabbitMQ HostName: {connectionFactory.HostName}");
-                            _logger.LogDebug($"RabbitMQ Port: {connectionFactory.Port}");
-                            _logger.LogDebug($"RabbitMQ UserName: {connectionFactory.UserName}");
-
-                            connection = connectionFactory.CreateConnection();
-                            channel = connection.CreateModel();
-
-                            channel.ExchangeDeclare("robot", ExchangeType.Direct, durable: false, autoDelete: true,
-                                arguments: null);
-                            channel.QueueDeclare("sensors", durable: false, exclusive: false, autoDelete: true,
-                                arguments: null);
-
-                            _logger.LogInformation("Established connection to RabbitMQ");
-                        }
-                        catch (Exception e)
-                        {
-                            _logger.LogDebug(e.Message);
-                            channel = null;
-                            connection = null;
-                        }
-                    }
-
-                    await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
+                    await ExecuteSensorLoopBody(stoppingToken);
                 }
             }
             catch (OperationCanceledException)
@@ -78,13 +39,86 @@ namespace kata_rabbitmq.robot.app
             }
             finally
             {
-                _logger.LogInformation("Shutting down ...");
+                ShutdownService();
+            }
+        }
 
-                channel?.Close();
-                connection?.Close();
-                
-                _logger.LogDebug("Shutdown complete.");
-            } 
+        private void RegisterCancellationRequest(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Waiting for cancellation request");
+            stoppingToken.Register(() => _logger.LogInformation("STOP request received"));
+            stoppingToken.ThrowIfCancellationRequested();
+        }
+
+        private async Task ExecuteSensorLoopBody(CancellationToken stoppingToken)
+        {
+            if (_channel == null)
+            {
+                ConnectToRabbitMq();
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
+        }
+
+        private void ConnectToRabbitMq()
+        {
+            try
+            {
+                _logger.LogDebug("Connecting to RabbitMQ ...");
+
+                var connectionFactory = CreateRabbitMqConnectionFactory();
+                _connection = connectionFactory.CreateConnection();
+                _channel = _connection.CreateModel();
+
+                _channel.ExchangeDeclare("robot", ExchangeType.Direct, durable: false, autoDelete: true,
+                    arguments: null);
+                _channel.QueueDeclare("sensors", durable: false, exclusive: false, autoDelete: true,
+                    arguments: null);
+
+                _logger.LogInformation("Established connection to RabbitMQ");
+            }
+            catch (Exception e)
+            {
+                _logger.LogDebug(e.Message);
+                _channel = null;
+                _connection = null;
+            }
+        }
+
+        private ConnectionFactory CreateRabbitMqConnectionFactory()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                VirtualHost = "/",
+                ClientProvidedName = "app:robot",
+                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOSTNAME"),
+                UserName = Environment.GetEnvironmentVariable("RABBITMQ_USERNAME"),
+                Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD")
+            };
+            var portString = Environment.GetEnvironmentVariable("RABBITMQ_PORT");
+            if (portString != null) connectionFactory.Port = int.Parse(portString);
+
+            _logger.LogDebug($"RabbitMQ HostName: {connectionFactory.HostName}");
+            _logger.LogDebug($"RabbitMQ Port: {connectionFactory.Port}");
+            _logger.LogDebug($"RabbitMQ UserName: {connectionFactory.UserName}");
+            return connectionFactory;
+        }
+
+        private void ShutdownService()
+        {
+            _logger.LogInformation("Shutting down ...");
+
+            DisconnectFromRabbitMq();
+
+            _logger.LogDebug("Shutdown complete.");
+        }
+
+        private void DisconnectFromRabbitMq()
+        {
+            _channel?.Close();
+            _connection?.Close();
+            _channel = null;
+            _connection = null;
         }
     }
 }
